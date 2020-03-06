4b0cf95cf1e6604ac3ee25c4ee01046f3aa1fb53
Tomer <tomerl101@gmail.com>
2020-03-07 01:29:01 +0200

    add incoming reports logs and moved cors headers middleware to cors.js file

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-require ('./database')
-require ('./server')
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
+const db = require ('./database');
+const express = require('express');
+const cors = require('./utils/cors');
+const userCtrl = require('./controllers/userCtrl');
+const objectCtrl = require('./controllers/objectCtrl');
+const geobjectCtrl = require('./controllers/geobjectCtrl');
+const deltaCtrl = require('./controllers/deltaCtrl');
+const ctrl = require('./controllers/ctrl');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+require('./utils/socketIO')(io);
+
+
+const port = process.env.PORT || 8080;
+
+app.set('port', port);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+app.use(morgan('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'logs/reports.log'), { flags: 'a' })
+}));
+
+app.use(cors);
+
+/*** All routes ***/
+app.get('/health', (req,res) => res.json("available"))
+app.get('/users', userCtrl.fetchAllUsers);
+app.get('/deploy', ctrl.fetchAllDeploy);
+app.get('/objects', objectCtrl.fetchAllObjects);
+app.get('/geobject', geobjectCtrl.fetchAllGeobjects);
+app.get('/deltas', deltaCtrl.fetchAllDeltas);
+
+http.listen(port, () => console.log(`listening on port ${port}`));
