982da5d92f73adb101f45cc267ec2ef122cb99d3
Netta Richer <nettaricher1@gmail.com>
2020-05-23 18:20:53 +0300

    fix turf area 5 and last

--- utils/mqDeltas.js
+++ utils/mqDeltas.js
@@ -127,27 +127,7 @@ amqp.connect('amqp://qfrftznl:gVWftNle39STIm0A2Gdclre7Nja4W5Qk@orangutan.rmq.clo
                     })
                     .then(result => {
                         if (result.length > 0) {
-                            let polygon = turf.polygon([
-                                                            [
-                                                                [arrayCoords[0],arrayCoords[1]],
-                                                                [arrayCoords[2],arrayCoords[3]],
-                                                                [arrayCoords[4],arrayCoords[5]],
-                                                                [arrayCoords[0],arrayCoords[1]]
-                                                            ]
-                                                        ])
-                            console.log("===========")
-                            console.log(polygon)
-                            console.log("=")
-                            console.log([
-                                [
-                                    [arrayCoords[0],arrayCoords[1]],
-                                    [arrayCoords[2],arrayCoords[3]],
-                                    [arrayCoords[4],arrayCoords[5]],
-                                    [arrayCoords[0],arrayCoords[1]]
-                                ]
-                            ])
-                            console.log("=")
-                            console.log(arrayCoords)
+                            let polygon = turf.polygon([arrayCoords])
                             console.log("---------" +turf.area(polygon)+ "-----------") 
                             deltas = new Deltas({
                                 deployId: `${result[0].deployId}`,
